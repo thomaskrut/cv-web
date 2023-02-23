32d0c9994463bf06052822649cf321983166f42a
thomaskrut <thomas.krut@yh.nackademin.se>
2023-02-23 09:24:18 +0100

    various changes to layout

--- script.ts
+++ script.ts
@@ -1,11 +1,7 @@
-function mouseOver(element) {
-	
-}
-
 function showHideRow(row) {
 
-
 	if (document.getElementById(row)!.style.display == "none") {
+		
 		document.getElementById(row)!.style.display="block";
 	} else {
 		document.getElementById(row)!.style.display="none";
