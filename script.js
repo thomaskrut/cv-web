f6878f6d78819253398c5bf402e0709ab3ccd1e8
thomaskrut <thomas.krut@yh.nackademin.se>
2023-02-23 08:14:09 +0100

    switch to typescript

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 function mouseOver(element) {
-	element.innerHTML=element.innerHTML + " bajs";
 }
-
 function showHideRow(row) {
-
-	if (document.getElementById(row).style.display=="none") {
-		document.getElementById(row).style.display="block";
-	} else {
-		document.getElementById(row).style.display="none";
-	}
-	
+    if (document.getElementById(row).style.display == "none") {
+        document.getElementById(row).style.display = "block";
+    }
+    else {
+        document.getElementById(row).style.display = "none";
+    }
 }
-
---
+++ script.ts
@@ -0,0 +1,15 @@
+function mouseOver(element) {
+	
+}
+
+function showHideRow(row) {
+
+
+	if (document.getElementById(row)!.style.display == "none") {
+		document.getElementById(row)!.style.display="block";
+	} else {
+		document.getElementById(row)!.style.display="none";
+	}
+	
+}
+
