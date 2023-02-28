a53c4825c4e08e208f43ea72c6db5c64e77117cd
thomaskrut <thomas.krut@yh.nackademin.se>
2023-02-28 22:04:22 +0100

    centered body, removed arrows

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var headings = document.querySelectorAll(".heading");
 console.log(headings);
 
 for (const h of headings) {
-    h.addEventListener("mousedown", function () {
+    h.addEventListener("mousedown", () => {
         toggleRow(h);
     });
     toggleRow(h);
@@ -19,10 +19,10 @@ function toggleRow(source) {
 
     if (elementToToggle.style.display == "none") {
         elementToToggle.style.display = "block";
-        source.innerHTML = "<h2>&#709; " + elementToToggle.id + "</h2>";
+        //source.innerHTML = "<h2>&#709; " + elementToToggle.id + "</h2>";
         } else {
         elementToToggle.style.display = "none";
-        source.innerHTML = "<h2>> " + elementToToggle.id + "</h2>";
+        //source.innerHTML = "<h2>> " + elementToToggle.id + "</h2>";
     }
     
 }
