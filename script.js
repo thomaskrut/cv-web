55bdcbfd2b1aaf15366c4e55da8272ec535eedc8
thomaskrut <thomas.krut@yh.nackademin.se>
2023-03-01 11:12:14 +0100

    experimenting with json import

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 "use strict";
 
+let data = fetch('./data.json')
+    .then((response) => response.json())
+    .then(data => {
+
+        console.log(data.intro.swe);
+
+        document.getElementById('intro').innerHTML = data.intro.swe;
+
+    })
+
+
+
+
 var headings = document.querySelectorAll(".heading");
 console.log(headings);
 
@@ -14,15 +27,15 @@ for (const h of headings) {
 function toggleRow(source) {
 
     const headingText = source.id;
-   
+
     const elementToToggle = document.getElementById(source.id.substring(0, source.id.length - 7));
 
     if (elementToToggle.style.display == "none") {
         elementToToggle.style.display = "block";
         //source.innerHTML = "<h2>&#709; " + elementToToggle.id + "</h2>";
-        } else {
+    } else {
         elementToToggle.style.display = "none";
         //source.innerHTML = "<h2>> " + elementToToggle.id + "</h2>";
     }
-    
+
 }
