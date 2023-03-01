2a891f201ff12dc940b01143ab0ea4891b7896de
thomaskrut <thomas.krut@yh.nackademin.se>
2023-03-01 13:56:03 +0100

    some language switching implemented

--- script.js
+++ script.js
@@ -1,6 +1,70 @@
 "use strict";
 
+loadContent('swe');
+
+var headings = document.querySelectorAll('.heading');
+console.log(headings);
+
+for (const h of headings) {
+    h.addEventListener("mousedown", () => {
+        toggleRow(h);
+    });
+    toggleRow(h);
+}
+
+let currentLanguage = 'swe';
+
+document.getElementById('languageToggle').addEventListener("mousedown", () => toggleLanguage());
+
+
+
+
+
+
+function toggleLanguage() {
+
+    if (currentLanguage == 'swe') {
+        document.getElementById('languageToggle').src = "english.png";
+        currentLanguage = 'eng'
+    }
+
+    else if (currentLanguage == 'eng') {
+        document.getElementById('languageToggle').src = "swedish.png";
+        currentLanguage = 'swe'
+    }
+
+    loadContent(currentLanguage);
+
+}
+
+function loadContent(language) {
+
+    let source = './content-swe.json';
+
+    if (language == 'swe') {
+        source = './content-swe.json'
+    }
+    if (language == 'eng') {
+        source = "./content-eng.json"
+    }
+
+    fetch(source)
+    .then((response) => response.json())
+    .then(data => {
+
+        document.getElementById('Intro').innerHTML = data.intro.text;
+
+        loadJsonToTable(data, 'education', 'Utbildning');
+        loadJsonToTable(data, 'work', 'Arbetslivserfarenhet');
+        loadJsonToTable(data, 'other', 'Övrigt');
+
+    });
+
+}
+
 function loadJsonToTable(dataObject, jsonObjectName, elementId) {
+
+    document.getElementById(elementId).innerHTML = "";
     for (const entry of dataObject[jsonObjectName]) {
 
         document.getElementById(elementId).innerHTML = document.getElementById(elementId).innerHTML + `
@@ -11,32 +75,6 @@ function loadJsonToTable(dataObject, jsonObjectName, elementId) {
     }
 }
 
-let data = fetch('./content.json')
-    .then((response) => response.json())
-    .then(data => {
-
-        document.getElementById('Intro').innerHTML = data.intro.swe;
-
-        loadJsonToTable(data, 'education', 'Utbildning');
-        loadJsonToTable(data, 'work', 'Arbetslivserfarenhet');
-        loadJsonToTable(data, 'other', 'Övrigt');
-
-    });
-
-
-
-
-var headings = document.querySelectorAll(".heading");
-console.log(headings);
-
-for (const h of headings) {
-    h.addEventListener("mousedown", () => {
-        toggleRow(h);
-    });
-    toggleRow(h);
-}
-
-//document.getElementById("educationHeading").addEventListener("mousedown", function () { showHideRow('education'); });
 function toggleRow(source) {
 
     const headingText = source.id;
@@ -45,10 +83,8 @@ function toggleRow(source) {
 
     if (elementToToggle.style.display == "none") {
         elementToToggle.style.display = "block";
-        //source.innerHTML = "<h2>&#709; " + elementToToggle.id + "</h2>";
     } else {
         elementToToggle.style.display = "none";
-        //source.innerHTML = "<h2>> " + elementToToggle.id + "</h2>";
     }
 
 }
